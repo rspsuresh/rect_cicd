47c5f3891ef1c4fce127e281cd1894c118be7ed7
rspsuresh <rsprampaul14321@gmail.com>
2026-08-02 08:04:50 +0530

    test commit

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    babel({ presets: [reactCompilerPreset()] })
+  ],
+  base:'/rect_cicd/',
+  server: {
+    port: 3000,
+    open: true,
+  },
+})
